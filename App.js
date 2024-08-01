1d14ea9b67f29bacded3b5984966e8b3baf35666
Fasin <fasingit17@gmail.com>
2024-08-01 12:40:54 +0530

    Make Welcome Page

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import WelcomeScreen from './app/screens/WelcomeScreen'; // Adjust the path as needed
+import {Text, SafeAreaView} from 'react-native'
+// import WelcomeScreen from './app/screens/WelcomeScreen';
+import InputScreen from './app/screens/InputNameScreen';
+
+
+const App = () => {
+  return <InputScreen/>;
+};
+
+export default App;
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,64 @@
+
+import React from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, StatusBar } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const WelcomeScreen = () => {
+  return (
+    <LinearGradient colors={['#2DAFD8', '#185D72']} style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <Image source={require('../assets/water_bottle_logo_welcome_page.png')} style={styles.image} />
+      <Text style={styles.appName}>Aguasync</Text>
+      <Text style={styles.description}>
+        Welcome to Aguasync! This tracks your water intake effortlessly.
+        Track your hydration progress and see how it impacts your overall health.
+      </Text>
+      <TouchableOpacity style={styles.button} onPress={() => { /* Handle the button press here */ }}>
+        <Text style={styles.buttonText}>Get Start</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 10,
+  },
+  appName: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    marginBottom: 20,
+  },
+  description: {
+    color: '#ffffff',
+    fontSize: 16,
+    textAlign: 'center',
+    marginHorizontal: 20,
+    position: 'absolute',
+    marginBottom: 20,
+    
+  },
+  button: {
+    backgroundColor: '#ffffff',
+    paddingVertical: 18,
+    paddingHorizontal: 84,
+    borderRadius: 25,
+    marginTop: 300
+  },
+  buttonText: {
+    color: '#007AFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default WelcomeScreen;
